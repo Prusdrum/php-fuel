2f24446410872ceede0787795122737c8dfbcc6b
prusdrum <mateusz.prusaczyk@gmail.com>
2019-02-28 16:06:53 +0100

    classes work

--- web/app/src/app.php
+++ web/app/src/app.php
@@ -2,11 +2,13 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+use Fuel\Model\GasStation;
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
   $name = $args['name'];
-  $response->getBody()->write("Hello, $name");
+  $station = new GasStation($name . " owns this gas station");
+  $response->getBody()->write($station->getName());
 
   return $response;
 });
